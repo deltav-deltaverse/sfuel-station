76e428e52e93fd609b8d68fe990c8e2294c45c54
TheGreatAxios <thegreataxios@thegreaiossimac.home>
2022-06-22 13:44:01 -0700

    Connected and Disconnected Gradient Added

--- src/screens/home/connected.tsx
+++ src/screens/home/connected.tsx
@@ -2,7 +2,7 @@ import { SectionContainer } from "../../components";
 
 const Connected = () => {
     return (
-        <SectionContainer backgroundColor="green">
+        <SectionContainer backgroundColor="linear-gradient(135deg, #DD1173, #350e47)">
             <p>Connected</p>
         </SectionContainer>
     );
