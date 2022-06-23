9cbb0ae6b22bf572350c9ca7c0a5875d03150d10
TheGreatAxios <thegreataxios@thegreaiossimac.home>
2022-06-22 18:10:22 -0700

    FUEL4.1 Balance Provider Setup and Faucet Integration Begun

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -6,6 +6,7 @@ import { Disconnected } from './disconnected';
 import { Gasless } from './gasless';
 import { MyLilius } from './mylilius';
 import { useEffect, useState } from 'react';
+import { SFuelFaucet } from '../logic/api';
 
 const Container = styled.div``;
 
@@ -13,12 +14,20 @@ const Container = styled.div``;
 const HomeScreen = () => {
     
     const [hasInterval, setHasInterval] = useState<boolean>(false);
+    const [sFuelAPI, setSFuelAPI] = useState<SFuelFaucet | undefined>(undefined);
+    
 
     const web3: Web3ContextType<Web3Provider> = useWeb3React();
     const { connector } = web3;
     
     const checkLogic = () => {
-        console.log("CHECK");
+        if (!sFuelAPI) {
+            console.log("Build API");
+            setSFuelAPI(new SFuelFaucet());
+        }
+
+        /// Make Call
+        console.log("MAKE CALL");
     }
 
     useEffect(() => {
